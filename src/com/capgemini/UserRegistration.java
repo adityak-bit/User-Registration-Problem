e846887f6056dcff62766eca2dfef7160b1f1df2
adityak-bit <adityajsr1998@gmail.com>
2020-10-03 00:12:39 +0530

    [ADITYA] Added : Last name Validator

--- src/com/capgemini/UserRegistration.java
+++ src/com/capgemini/UserRegistration.java
@@ -6,20 +6,20 @@ import java.util.regex.*;
 public class UserRegistration {
 	public static void main(String[] args) {
 		System.out.println("Welcome to User Registration System");
-		System.out.println("Enter user first name");
+		System.out.println("Enter user last name");
 
 		Scanner sc = new Scanner(System.in);
-		String firstName = sc.nextLine();
+		String lastName = sc.nextLine();
 
-		Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{2,}");
-		Matcher matcher = pattern.matcher(firstName);
+		Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{2,}$");
+		Matcher matcher = pattern.matcher(lastName);
 		boolean result = matcher.matches();
 
 		if (result == true) {
-			System.out.println("Valid user first name");
+			System.out.println("Valid user last name");
 		} else {
-			System.out.println("Invalid user first name");
-			System.out.println("First name starts with uppercase and has minimum 3 characters");
+			System.out.println("Invalid user last name");
+			System.out.println("Last name starts with uppercase and has minimum 3 characters");
 		}
 	}
 
