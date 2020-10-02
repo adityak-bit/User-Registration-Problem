6900f934727e08f9fb25e5d0e7b72a4a5704b3b6
adityak-bit <adityajsr1998@gmail.com>
2020-10-03 00:04:17 +0530

    [ADITYA] Added : Welcome message

---
+++ src/com/capgemini/UserRegistration.java
@@ -0,0 +1,11 @@
+package com.capgemini;
+
+import java.util.*;
+import java.util.regex.*;
+
+public class UserRegistration {
+	public static void main(String[] args) {
+		System.out.println("Welcome to User Registration System");
+	}
+
+}
