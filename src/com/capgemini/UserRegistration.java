9eba6212fc9fd11194b58bd8aa13fc8c3247846c
adityak-bit <adityajsr1998@gmail.com>
2020-10-04 12:12:54 +0530

    [ADITYA] Added : password- rule 2 validator

--- src/com/capgemini/UserRegistration.java
+++ src/com/capgemini/UserRegistration.java
@@ -12,15 +12,16 @@ public class UserRegistration       {
 
 		String password = sc.nextLine();
 
-		Pattern pattern = Pattern.compile("^[A-Za-z0-9.-_+@*]{8,}");
+		Pattern pattern = Pattern.compile("(?=[A-Z])([Sa-z0-9.-_+@*]){8,}");
 		Matcher matcher = pattern.matcher(password);
 		boolean result = matcher.matches();
 
 		if (result == true) {
-			System.out.println("Password : Rule 1 satisfied");
+			System.out.println("Password : Rule 1 and 2 satisfied");
 		} else {
 			System.out.println("Invalid user password");
 			System.out.println("Password must be of minimum 8 characters");
+			System.out.println("Password must have atleast one uppercase character");
 		}
 	}
 
